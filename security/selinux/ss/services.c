955096f8b10ee82e156afa1559d2f1e43a70b4ce
kdrag0n <dragon@khronodragon.com>
2018-10-24 19:09:01 -0700

    selinux: don't require auditing
    Audit is bad. It affects performance under all circumstances. It's also
    dirty.
    
    From LWN: "Andy submitted a patch to fix this particular problem, but he
    didn't stop there. He has come to the conclusion that the audit
    subsystem is beyond repair, so his patch marks the whole thing as being
    broken, making it generally inaccessible. He cited a number of problems
    beyond this security issue: it hurts performance even when it is not
    being used, it is not (in his mind) reliable, it has problems with
    various architectures, and "its approach to freeing memory is
    terrifying." All told, Andy said, we're better off without it"
    
    Signed-off-by: kdrag0n <dragon@khronodragon.com>
    (cherry picked from commit ddd30747bf4997755fed21f3bbaa0ea41c5da7aa)
    (cherry picked from commit 7299c7f78f1913be988a7b055bc1568b41709a4e)
    (cherry picked from commit 74335f4ca8bdef50ab4b35796c04bf11dde684f3)
    (cherry picked from commit db3d1227340d6ef90a91741489d146527fc0e300)
    (cherry picked from commit bd0cf8321e5cc36206b1f0295941a8762eec1fe8)

--- security/selinux/ss/services.c
+++ security/selinux/ss/services.c
@@ -3447,6 +3447,7 @@ out:
 	return match;
 }
 
+#ifdef CONFIG_AUDIT
 static int (*aurule_callback)(void) = audit_update_lsm_rules;
 
 static int aurule_avc_callback(u32 event)
@@ -3469,6 +3470,7 @@ static int __init aurule_init(void)
 	return err;
 }
 __initcall(aurule_init);
+#endif
 
 #ifdef CONFIG_NETLABEL
 /**
